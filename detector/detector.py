d38bbab7b83597066dc51513b471d81e339bd855
GreeChel <maksancel032@gmail.com>
2024-06-13 14:57:43 +0300

    Configure config.json and update detector.py
    * Add header for functions from detector.py
    * Add function read_config(name):
    	- read json fole: config.json
    	- retern dict from config_data[name]
    * Update config.json for start work in detector.py

--- detector/detector.py
+++ detector/detector.py
@@ -1,12 +1,35 @@
 import cv2 as cv
 import numpy as np
+import json
+
+
+def read_config(name: str):
+    """
+    A function that opens and outputs values from the config.json file
+    Example:
+    read_config("canny") -> {'key1': num1, 'key2', num2}
+    """
+
+    # Read config.json -> data
+    data = {}
+    with open("config.json", 'r', encoding="utf-8") as f:
+        data = json.load(f)
+
+    # Output data[name]
+    return data[name]
 
 
 def find_candidates(img: np.ndarray):
-    pass
+    """
+    A function that outputs a list of 3 taps - [x, y, r]
+    """
+    # Convert image to GRAY
 
 
 def show_candidates(img, cands):
+    """
+    shows the picture where find_candidates was found
+    """
     pass
 
 
@@ -24,3 +47,9 @@ def find_contours(img, targ):
 
 def show_contours(pmap):
     pass
+
+
+# Entry point for quick tests
+if __name__ == "__main__":
+    data = read_config("canny")
+    print(data)
